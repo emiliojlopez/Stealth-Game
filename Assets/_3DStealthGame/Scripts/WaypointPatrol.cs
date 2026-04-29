aa858fd41d195c35574773a136e85d29fe6ecab5
jo090313 <jo090313@ucf.edu>
2026-04-29 03:26:07 -0400

    Minor Mod: Enemy movement
    - More enemies around the level overall
    - New behavior adds random wait times between moving to new waypoints

--- Assets/_3DStealthGame/Scripts/WaypointPatrol.cs
+++ Assets/_3DStealthGame/Scripts/WaypointPatrol.cs
@@ -9,11 +9,16 @@ public class WaypointPatrol : MonoBehaviour
     public Transform[] waypoints;
 
     private Rigidbody m_RigidBody;
+    //Tracks waiting timer
+    private float waitTimer = 0f;
+    //Amount of time that the enemy will wait
+    private float waitTime;
     int m_CurrentWaypointIndex;
 
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody>();
+        waitTime = Random.Range(1f, 3f);
     }
 
     void FixedUpdate()
@@ -21,9 +26,27 @@ public class WaypointPatrol : MonoBehaviour
         Transform currentWaypoint = waypoints[m_CurrentWaypointIndex];
         Vector3 currentToTarget = currentWaypoint.position - m_RigidBody.position;
 
+        //If the enemy is close enough to the target...
         if (currentToTarget.magnitude < 0.1f)
         {
-            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            //Every frame adds to the timer
+            waitTimer += Time.deltaTime;
+            //If the enemy as waited long enough...
+            if (waitTimer >= waitTime)
+            {
+                int nextIndex;
+                //Do while loop chooses a new waypoint, excluding the current one
+                do
+                {
+                    nextIndex = Random.Range(0, waypoints.Length);
+                } while (nextIndex == m_CurrentWaypointIndex);
+                //New waypoint
+                m_CurrentWaypointIndex = nextIndex;
+                //Resets timer and makes new random wait time
+                waitTimer = 0f;
+                waitTime = Random.Range(1f, 3f);
+            }
+            return;
         }
         Quaternion forwardRotation = Quaternion.LookRotation(currentToTarget);
         m_RigidBody.MoveRotation(forwardRotation);
