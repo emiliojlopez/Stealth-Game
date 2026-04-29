62108f6339ba1b242891a5fadbaeb8885987615c
emiliojlopez <emiliolopez21207@gmail.com>
2026-04-29 01:50:31 -0400

    Sprinting Stamina
    Added sprinting Stamina System with icon in the bottom center. Holding Shift while walking to sprint drains stamina. Stamina drain is shown on the icon. Once stamina is fully drained, the player can no longer sprint. Stamina won't regen until the player stops moving or lets go of shift

--- Assets/_3DStealthGame/Scripts/PlayerMovement.cs
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,9 +12,15 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed = 1.0f;
     public float turnSpeed = 20f;
     public float sprintSpeed = 1.75f;
-    public bool isShiftHeld;
+    public float Stamina;
+    public float MaxStamina;
+    public float StaminaDrain;
+    private bool HasStamina = true;
+    private bool HoldingShift = false;
+    public float ChargeRate;
     private float currentSpeed;
-    
+    public Image StaminaBar;
+    private Coroutine recharge;
 
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
@@ -28,17 +35,68 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        isShiftHeld = Input.GetKey(KeyCode.LeftShift);
+        if (Stamina > 0)
+        {
+            HasStamina = true;
+        }
+        HoldingShift = (Input.GetKey(KeyCode.LeftShift));
         // Check if sprinting
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            currentSpeed = sprintSpeed;
+            var pos = MoveAction.ReadValue<Vector2>();
+
+            float horizontal = pos.x;
+            float vertical = pos.y;
+
+            m_Movement.Set(horizontal, 0f, vertical);
+            m_Movement.Normalize();
+
+
+            bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+            bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+            bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+            if (isWalking == true)
+            {
+                if (recharge != null) StopCoroutine(recharge);
+                recharge = StartCoroutine(RechargeStamina());
+
+                if (HasStamina == true)
+                {
+                    currentSpeed = sprintSpeed;
+                    Stamina -= StaminaDrain * Time.deltaTime;
+                    StaminaBar.fillAmount = Stamina / MaxStamina;
+                    if (Stamina <= 0)
+                    {
+                        Stamina = 0;
+                        currentSpeed = walkSpeed;
+                        HasStamina = false;
+                    }
+                }
+                else
+                {
+                    currentSpeed = walkSpeed;
+                }
+            }
         }
         else
         {
             currentSpeed = walkSpeed;
         }
     }
+    private IEnumerator RechargeStamina ()
+    {
+        yield return new WaitForSeconds(1f);
+
+        while (Stamina < MaxStamina)
+        {
+            Stamina += ChargeRate / 10f;
+            if (Stamina > MaxStamina) Stamina = MaxStamina;
+            StaminaBar.fillAmount = Stamina / MaxStamina;
+            yield return new WaitForSeconds(.1f);
+        }
+        }
+
 
         void FixedUpdate()
     {
@@ -54,10 +112,11 @@ public class PlayerMovement : MonoBehaviour
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        bool isSprinting = isShiftHeld && isWalking;
+        bool TryingSprint = HasStamina && HoldingShift;
+        bool isSprinting = TryingSprint && isWalking;
         m_Animator.SetBool("IsSprinting", isSprinting);
         m_Animator.SetBool("IsWalking", isWalking);
-       
+
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
